fb99e6c5068bfa814eacca6d132e821ab93ee3ee
khalidur-sujon <khalidur.sujon@gmail.com>
2024-05-29 17:13:15 +0600

    Automatic changes after installed shadcn lib

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,22 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
 	],
+	prefix: "",
 	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
 		extend: {
 			screens: {
 				xs: "400px",
@@ -38,8 +48,23 @@ const config: Config = {
 				white100: "#F7F9FC",
 				white200: "#F6F7F9",
 			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
 		},
 	},
-	plugins: [],
-};
+	plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
