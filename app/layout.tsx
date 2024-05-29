a4fdb104d1d214535fcbdd6093cfb526a6704b4e
khalidur-sujon <khalidur.sujon@gmail.com>
2024-05-29 14:25:05 +0600

    Initialize Project

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Rent Wheels",
+	description: "RentWheels - A car booking site",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
