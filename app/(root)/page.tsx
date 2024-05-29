73f9d253490eac63776f6cadf351d8067e3a0013
khalidur-sujon <khalidur.sujon@gmail.com>
2024-05-29 21:59:29 +0600

    feat: AdvertDisplay component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import AdvertsDisplay from "@/components/advertsComponents/AdvertsDisplay";
+
 const page = () => {
-	return <div>page</div>;
+	return (
+		<main className="flex flex-col items-center p-2  bg-white200 dark:bg-gray900">
+			<div className="mt-24 w-full max-w-[90rem] flex flex-col  items-center pt-5 ">
+				<AdvertsDisplay />
+			</div>
+		</main>
+	);
 };
 
 export default page;
