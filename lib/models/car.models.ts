919c3a90aee4a2b5cf3a12d2cbcd8be8b3a636b7
khalidur-sujon <khalidur.sujon@gmail.com>
2024-05-30 12:12:59 +0600

    add: db schema for car

---
+++ lib/models/car.models.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const carSchema = new mongoose.Schema(
+	{
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		reviews: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Review",
+			},
+		],
+		carTitle: {
+			type: String,
+			required: true,
+		},
+		carType: {
+			type: String,
+			required: true,
+		},
+		disabledDates: {
+			singleDates: [Date],
+			dateRanges: [
+				{
+					from: Date,
+					to: Date,
+				},
+			],
+		},
+		likes: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+		carRented: Number,
+		averageRating: Number,
+		rentPrice: String,
+		capacity: String,
+		transmission: String,
+		location: String,
+		fuelCapacity: String,
+		shortDescription: String,
+		carImages: [String],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Car = mongoose?.models?.Car || mongoose.model("Car", carSchema);
+
+export default Car;
