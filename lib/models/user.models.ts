ad25ce8f5ec088a82a272ed1d7f49640f4c9f0f0
khalidur-sujon <khalidur.sujon@gmail.com>
2024-05-30 12:13:21 +0600

    add: db schema for user

---
+++ lib/models/user.models.ts
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+	{
+		id: {
+			type: String,
+			unique: true,
+			required: true,
+		},
+		username: {
+			type: String,
+			unique: true,
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: false,
+		},
+		image: {
+			type: String,
+			required: false,
+		},
+		coverImage: {
+			type: String,
+			required: false,
+		},
+		bio: {
+			type: String,
+			required: false,
+		},
+		onboarded: {
+			type: Boolean,
+			default: false,
+		},
+		carsAdded: [
+			{
+				car: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "Car",
+				},
+			},
+		],
+		carsRented: [
+			{
+				car: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "Car",
+					required: true,
+				},
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const User = mongoose?.models?.User || mongoose.model("User", userSchema);
+
+export default User;
